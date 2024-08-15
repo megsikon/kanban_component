c16829e727df0d0425e622f0f1cc2295f198dd43
Magnus Emil Gervig Svensson <mesv@itu.dk>
2024-08-15 11:05:40 +0200

    Setup

---
+++ kanban_component/test/test.test.ts
@@ -0,0 +1,10 @@
+export function sum(a, b) {
+  return a + b
+}
+
+import { expect, test } from 'vitest'
+//import { sum } from './sum.js'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
